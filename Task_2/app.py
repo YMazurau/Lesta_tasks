2ac77c3b83d9c5d80a3eb9093f2c321d42888edc
YMazurov <mazurov1804@gmail.com>
2025-04-13 17:21:13 +0300

    app ed

--- Task_2/app.py
+++ Task_2/app.py
@@ -4,7 +4,7 @@ import os
 
 app = Flask(__name__)
 
-redis_host = os.environ.get('REDIS_HOST', 'localhost')
+redis_host = os.getenv('REDIS_HOST', 'localhost')
 redis_client = redis.Redis(host=redis_host, port=6379, db=0, decode_responses=True)
 
 @app.route('/ping', methods=['GET'])
