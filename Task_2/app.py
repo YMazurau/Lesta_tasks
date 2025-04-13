e1f8ef72e792b8397431ded8b48b29c3c0a9434f
YMazurov <mazurov1804@gmail.com>
2025-04-13 16:31:11 +0300

    task2

---
+++ Task_2/app.py
@@ -0,0 +1,21 @@
+from flask import Flask, jsonify
+import redis
+import os
+
+app = Flask(__name__)
+
+redis_host = os.environ.get('REDIS_HOST', 'localhost')
+redis_client = redis.Redis(host=redis_host, port=6379, db=0, decode_responses=True)
+
+@app.route('/ping', methods=['GET'])
+def ping():
+    return jsonify({"status": "ok"})
+
+@app.route('/count', methods=['GET'])
+def count():
+    count = r.incr('counter')
+    return jsonify({"count": count})
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
+
